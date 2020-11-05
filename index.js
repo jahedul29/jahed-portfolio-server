16c882ddfde28fbadf615b5ddc0383908ccf021c
[jahedul29] <[jahedulh1@gmail.com]>
2020-11-05 21:26:18 +0600

    changing port

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(fieUpload());
 
-// const port = process.env.PORT || 5000;
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
